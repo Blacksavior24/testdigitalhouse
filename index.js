f75c5c457a0cc1d1e510fa2ee81460eae4c56396
Emerson Villalta <reaperfocs@gmail.com>
2023-06-24 05:18:17 -0500

    Commit para el github

---
+++ index.js
@@ -0,0 +1,9 @@
+//Un poco de js para mejorar el móvil XD
+
+const burguerIcon = document.getElementById('burguer');
+
+burguerIcon.addEventListener('click', function() {
+    console.log("CLick");
+    //Un poco de css para alegrar el día, y que fue hecho por mí
+    alert("Entrar a mi portafolio XD  https://portfolioevq.onrender.com")
+});
